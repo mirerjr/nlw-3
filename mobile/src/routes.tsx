44bad2120b7ac538584ab913ef9e59c8f0ee0aa9
Mirer Junior <42882594+mirerjr@users.noreply.github.com>
2020-10-20 17:57:28 -0300

    Aula 4 - mobile

---
+++ mobile/src/routes.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import OrphanagesMap from './pages/OrphanagesMap';
+import OrphanagesDetails from './pages/OrphanagesDetails';
+
+const { Navigator, Screen} = createStackNavigator();
+
+export default function Routes(){
+    return(
+        <NavigationContainer>
+            <Navigator screenOptions={{ headerShown: false }}>
+                <Screen name="OrphanagesMap" component={OrphanagesMap} 
+                //options={{ headerShown: false }}
+                />
+                <Screen name="OrphanagesDetails" component={OrphanagesDetails} 
+                //options={{ headerShown: false }}
+                />
+            </Navigator>
+        </NavigationContainer>
+    );
+}
